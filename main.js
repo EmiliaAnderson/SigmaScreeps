f5a915165ab5c7862578acfa87a13dddf59d3f04
patriciag <patriciag@allrecipes.com>
2018-05-31 11:48:55 -0700

    Creating roleWorker

--- main.js
+++ main.js
@@ -1,31 +1,23 @@
+var roleWorker = require('role.worker');
+
 module.exports.loop = function () {
-  var spawn = Game.spawns['Spawn1'];
-  console.log("Running loop");
+    var spawn = Game.spawns['Spawn1'];
+    console.log("Running loop");
 
-  if (spawn.room.energyAvailable >= 200) {
+    if (spawn.room.energyAvailable >= 200) {
     console.log('Trying to spawn!')
-      spawn.spawnCreep([WORK,CARRY,MOVE], 'bob3', {memory:{role: 'worker'}});
-  }
-  else {
-      console.log("Not enough energy!");
-  }
+        spawn.spawnCreep([WORK,CARRY,MOVE], 'bob3', {memory:{role: 'worker'}});
+    }
+    else {
+        console.log("Not enough energy!");
+    }
 
-  for(var name in Game.creeps) {
-      var creep = Game.creeps[name];
+    var sources = creep.room.find(FIND_SOURCES);
+    var mainSource = sources[0];
 
-      if(creep.carry.energy < creep.carryCapacity){
-          var sources = creep.room.find(FIND_SOURCES);
-      
-          if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-              creep.moveTo(sources[0]);
-          }
-      }
-      else {
-          if( creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
-              creep.moveTo(spawn);
-          }
-      }
-  }
-
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        roleWorker.run(spawn, mainSource);        
+    }
 }
 
