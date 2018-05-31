9fb65446ce98c8fb7d394a5cb43cc9b876fc6144
Skyler Murray <smurray@meredith.com>
2018-05-31 11:34:57 -0700

    Updated to add logs

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 module.exports.loop = function () {
-    var spawn = Game.spawns['Spawn1'];
+  var spawn = Game.spawns['Spawn1'];
+  console.log("Running loop");
 
-    if (spawn.room.energyAvailable >= 200) {
-        spawn.spawnCreep([WORK,CARRY,MOVE], undefined, {role: 'worker'});
-    }
+  if (spawn.room.energyAvailable >= 200) {
+    console.log('Trying to spawn!')
+      spawn.spawnCreep([WORK,CARRY,MOVE], 'bob3', {memory:{role: 'worker'}});
+  }
+  else {
+      console.log("Not enough energy!");
+  }
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
+  for(var name in Game.creeps) {
+      var creep = Game.creeps[name];
 
-        if(creep.carry.energy < creep.carryCapacity){
-            var sources = creep.room.find(FIND_SOURCES);
-        
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
-        }
-        else {
-            if( creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
-                creep.moveTo(spawn);
-            }
-        }
-    }
+      if(creep.carry.energy < creep.carryCapacity){
+          var sources = creep.room.find(FIND_SOURCES);
+      
+          if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(sources[0]);
+          }
+      }
+      else {
+          if( creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
+              creep.moveTo(spawn);
+          }
+      }
+  }
 
 }
 
